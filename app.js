9a6df18e06b2d248a1fc58e23303579797ff5615
razzak-1 <71546699+razzak-1@users.noreply.github.com>
2021-11-12 18:28:02 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var name=prompt('name');
+var occupation=prompt('Your aim in life plz');
+console.log('I am '+name+' & I wAnt to be a '+occupation);
+
+var name=prompt('name');
+var occupation=prompt('Your aim in life plz');
+console.log('I am '+name+' & I wAnt to be a '+occupation);
